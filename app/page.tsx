248166b58582e43b25c471d983dc1d3d8f95eeaf
Cristianmoreno25 <cristiangmoreno2402@gmail.com>
2025-10-17 11:22:21 -0500

    ulimas actualizaciones

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,5 @@ import { redirect } from "next/navigation";
 
 export default function Home() {
   // Redirige a public/html/login.html
-  redirect("html/login.html");
+  redirect("html/Index.html");
 }
